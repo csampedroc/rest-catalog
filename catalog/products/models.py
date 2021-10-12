b74d28e87f6aaeb0ae588cca7fcbcb9bc48d90d5
Carlos Sampedro <carlos.sampedroc@gmail.com>
2021-10-12 12:37:44 -0500

    feat: :construction: Created brand and product models

--- catalog/products/models.py
+++ catalog/products/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
 
 # Create your models here
+
+class Brand(models.Model):
+    name = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    sku = models.CharField(max_length=200)
+    name = models.CharField(max_length=300)
+    price = models.FloatField()
+    queries = models.IntegerField()
+    id_brand = models.ForeignKey(Brand, related_name='brands', on_delete=models.RESTRICT)
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey('auth.User', related_name='products', on_delete=models.RESTRICT)
+
+    def __str__(self):
+        return f'{self.sku} {self.name}'
