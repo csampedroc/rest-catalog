8949de7e364dc4d9cbf7f0a999b6a5799d7a572f
Carlos Sampedro <carlos.sampedroc@gmail.com>
2021-10-13 09:52:07 -0500

    feat: :sparkles: Added Brand and User views

--- catalog/products/views.py
+++ catalog/products/views.py
@@ -1,16 +1,28 @@
 from django.contrib.auth.models import User
+from django.db.models import query
 from rest_framework import generics, serializers
 
 from .models import Brand, Product
-from .serializers import BrandSerializer, ProductSerializer
+from .serializers import BrandSerializer, ProductSerializer, UserSerializer
 
 
 class ProductList(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-
 class ProductDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+class BrandList(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+class BrandDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 
