f804c74c2ecbfed50675a0388a851430b421bb89
Carlos Sampedro <carlos.sampedroc@gmail.com>
2021-10-15 12:20:32 -0500

    test: :white_check_mark: Added model tests

--- catalog/products/tests.py
+++ catalog/products/tests.py
@@ -1,3 +1,24 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Brand, Product
+
+
+class TestBrandModel(TestCase):
+    def setUp(self):
+        Brand.objects.create(name="Brand Test")
+
+    def test_brand_create(self):
+        brand_result = Brand.objects.last()
+        assert str(brand_result) == "Brand Test"
+
+
+class TestProductModel(TestCase):
+    def setUp(self):
+        brand = Brand.objects.create(name="Brand Test")
+        user = User.objects.create(username="user1", password="user1@com", email="user@gmail.com")
+        Product.objects.create(sku="PROD-001", name="Product Test 1", price=20, id_brand=brand, created_by=user)
+
+    def test_product_create(self):
+        product_result = Product.objects.last()
+        assert str(product_result) == "PROD-001 Product Test 1"
--- catalog/products/urls.py
+++ catalog/products/urls.py
@@ -5,8 +5,8 @@ from django.urls.conf import path
 urlpatterns = [
     path("products/", views.ProductList.as_view()),
     path("products/<int:pk>/", views.ProductDetail.as_view()),
-    path("brands/", views.BrandList.as_view()),
-    path("brands/<int:pk>/", views.BrandDetail.as_view()),
+    path("brands/", views.BrandList.as_view(), name="brands-list"),
+    path("brands/<int:pk>/", views.BrandDetail.as_view(), name="brands-detail"),
     path("users/", views.UserList.as_view()),
     path("users/<int:pk>/", views.UserDetail.as_view()),
 ]
