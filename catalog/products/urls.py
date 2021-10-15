dedf50a7aecfa5b9dc0385f87ec71f1f8fb20db4
Carlos Sampedro <carlos.sampedroc@gmail.com>
2021-10-15 12:47:42 -0500

    feat: :construction: Updated url names for testing purposes

--- catalog/products/urls.py
+++ catalog/products/urls.py
@@ -3,8 +3,8 @@ from django import urls
 from django.urls.conf import path
 
 urlpatterns = [
-    path("products/", views.ProductList.as_view()),
-    path("products/<int:pk>/", views.ProductDetail.as_view()),
+    path("products/", views.ProductList.as_view(), name="products-list"),
+    path("products/<int:pk>/", views.ProductDetail.as_view(), name="products-detail"),
     path("brands/", views.BrandList.as_view(), name="brands-list"),
     path("brands/<int:pk>/", views.BrandDetail.as_view(), name="brands-detail"),
     path("users/", views.UserList.as_view()),
